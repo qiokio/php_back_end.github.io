eec8ef4197b4203e568793b9d72aa4b3b035099e
qiokio <59764006+qiokio@users.noreply.github.com>
2020-02-23 20:14:29 -0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$host='http://qiokio.github.io['HTTP_HOST'].'/';
+//本站域名
+
+$typearr=array("png","jpg","txt","text","html","js","mp4","vdat");
+//自定义文件格式类型，要关联type.php文件
+
+
+define("HOST",$host);
+
+define("TYPE",$typearr);
+
+?>
